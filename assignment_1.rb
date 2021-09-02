94d564792da0dc5c359dfdd25da2c72d414514b5
Kanin Tansirisittikul <61454485+t-kanin@users.noreply.github.com>
2021-09-02 13:54:53 +0700

    add assignment_1

---
+++ assignment_1.rb
@@ -0,0 +1,33 @@
+def solution(number) 
+
+	arr = Array.new 
+
+	return arr << number if number < 10
+
+	while number != 0 do 
+		arr << number % 10 
+		number = number / 10 
+	end 
+
+	arr = arr.sort.reverse
+	res = arr.join.to_i
+	return res
+end 
+
+#test 4 digits
+p test1 = solution(9687) 
+p test2 = solution(2000)
+p test3 = solution(5445)
+
+# test 1 digit 
+p test4 = solution(3)
+p test0 = solution(0) 
+
+# test 2 digits
+p test5 = solution(45)
+
+
+
+
+
+
