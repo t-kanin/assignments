e199bb399fa4f42fbdbe9249b768c4dd2e5ac2f2
t.kanin <tan.kanin98@gmail.com>
2021-09-03 16:19:09 +0700

    refactor code

--- assignment_3.rb
+++ assignment_3.rb
@@ -1,25 +1,28 @@
+def calculate(s, v, k)
+  s += k 
+  v -= 1
+  return s, v
+end 
+
 def solution(a,b,c) 
   s = ''
   count = 0
   total = a + b + c 
-  hash = { 'a' => a, 'b' => b, 'c' => c}
-  hash_sort = hash.sort_by { |k, v| -v}
+  hash = { 'a' => a, 'b' => b, 'c' => c }
+  hash_sort = hash.sort_by { |k, v| -v }
   k_max, v_max = hash_sort.first 
   k_mid, v_mid = hash_sort[1]
   k_min, v_min = hash_sort.last
 
   for i in 0...total do 
     if count < 2 && v_max > 0 
-      s += k_max
-      v_max -= 1 
+      s, v_max = calculate(s, v_max, k_max)
       count += 1 
     elsif v_mid > 0
-      s += k_mid 
-      v_mid -= 1 
+      s, v_mid = calculate(s, v_mid, k_mid)
       count = 0 
     elsif v_min > 0 
-      s += k_min 
-      v_min -= 1 
+      s, v_min = calculate(s, v_min, k_min)
       count = 0 
     else 
       break 
