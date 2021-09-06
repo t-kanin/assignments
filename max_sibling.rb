e1d6406297f3e19fb4cc4db61bda67e7362ea0dc
Kanin Tansirisittikul <61454485+t-kanin@users.noreply.github.com>
2021-09-06 10:17:55 +0700

    add assignment_1 (#1)
    * add assignment_1
    
    * fix empty lines and indentation
    
    * add test/expect result + simplify solution method
    
    * fixed extra lines, incorrect spaces
    
    * fixed? spacing
    
    * try update vscode setting
    
    * test spacing
    
    * fixed ? spacing/ update setting
    
    * test spacing
    
    * fixed spacing finally?
    
    * fixed spacing
    
    * refactor code, fixed code errors
    
    * update format
    
    * change file name, make it into class

---
+++ max_sibling.rb
@@ -0,0 +1,20 @@
+class MaxSibling
+  def solution(number)
+    return number if number < 10
+
+    find_max(number)
+  end
+
+  private
+
+  def find_max(number)
+    arr = []
+    loop do
+      break if number.zero?
+
+      arr << number % 10
+      number /= 10
+    end
+    arr.sort.reverse.join.to_i
+  end
+end
