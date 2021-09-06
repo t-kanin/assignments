42d20789879bc9d7d96fa8bed4ef3df9979d34bd
Kanin Tansirisittikul <tan.kanin98@gmail.com>
2021-09-06 21:53:21 +0700

    add class init, better naming style

--- day_of_week.rb
+++ day_of_week.rb
@@ -1,6 +1,14 @@
 class DayOfWeek
+  attr_accessor :day, :k
+
   DAYS = %w[Mon Tue Wed Thu Fri Sat Sun].freeze
-  def solution(day, k)
+
+  def initialize(day, k)
+    @day = day
+    @k = k
+  end
+
+  def solution
     index = DAYS.index(day)
     index = (index + k) % 7
     DAYS[index]
--- spec/day_of_week_spec.rb
+++ spec/day_of_week_spec.rb
@@ -3,44 +3,49 @@ require './day_of_week'
 
 describe DayOfWeek do
   describe '#solution' do
-    let(:instance) { described_class.new }
+    subject { instance.solution }
+    let(:instance) { described_class.new('Wed', 2) }
 
-    context 'given example' do
-      it 'should return Fri given s = Wed and k = 2' do
-        subject = instance.solution('Wed', 2)
+    context 'with given example' do
+      it 'returns Fri given s = Wed and k = 2' do
         expect(subject).to match('Fri')
       end
 
-      it 'should return Mon given s = Sat and k = 23' do
-        subject = instance.solution('Sat', 23)
+      it 'returns Mon given s = Sat and k = 23' do
+        instance.day = 'Sat'
+        instance.k = 23
         expect(subject).to match('Mon')
       end
     end
 
-    context 'given same day' do
-      it 'should return itself' do
-        subject = instance.solution('Mon', 0)
+    context 'with same day' do
+      it 'returns itself' do
+        instance.day = 'Mon'
+        instance.k = 0
         expect(subject).to match('Mon')
       end
     end
 
-    context 'given same week' do
-      it 'should return Wed given s = Mon and k = 2' do
-        subject = instance.solution('Mon', 2)
+    context 'within same week' do
+      it 'returns Wed given s = Mon and k = 2' do
+        instance.day = 'Mon'
+        instance.k = 2
         expect(subject).to match('Wed')
       end
     end
 
-    context 'given next week k < 7' do
-      it 'should return Wed given s = Sun and k = 3' do
-        subject = instance.solution('Sun', 3)
+    context 'with next week where k < 7' do
+      it 'returns Wed given s = Sun and k = 3' do
+        instance.day = 'Sun'
+        instance.k = 3
         expect(subject).to match('Wed')
       end
     end
 
-    context 'given Big k >= 7' do
-      it 'should return Sat given s = Wed and k = 500' do
-        subject = instance.solution('Wed', 500)
+    context 'with Big where k >= 7' do
+      it 'returns Sat given s = Wed and k = 500' do
+        instance.day = 'Wed'
+        instance.k = 500
         expect(subject).to match('Sat')
       end
     end
