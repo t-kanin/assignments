9926694c140ea8384e09db11c791e9c0a43ae97c
Kanin Tansirisittikul <61454485+t-kanin@users.noreply.github.com>
2021-09-06 11:12:49 +0700

    Assignment 2 (#2)
    * add assignment_1
    
    * init assignment_2
    
    * add assignment 2
    
    * Delete assignment_1.rb
    
    * fixed spacing
    
    * delete comments
    
    * update from suggestion + update traverse func
    
    * add comment
    
    * refactor code
    
    * change file name + refactor code

---
+++ tree_visibility.rb
@@ -0,0 +1,27 @@
+class TreeNode
+  attr_accessor :val, :l, :r
+
+  def initialize(val, l = nil, r = nil)
+    @val = val
+    @l = l
+    @r = r
+  end
+end
+
+class Tree
+  def traverse(tree, max)
+    tree.val >= max ? calculate_sum(1, tree, tree.val) : calculate_sum(0, tree, tree.val)
+  end
+
+  def calculate_sum(sum, tree, val)
+    sum += traverse(tree.l, val) unless tree.l.nil?
+    sum += traverse(tree.r, val) unless tree.r.nil?
+    sum
+  end
+
+  def solution(tree)
+    return 0 if tree.val.nil?
+
+    traverse(tree, tree.val)
+  end
+end
