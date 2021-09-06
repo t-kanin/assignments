9b23591ca3843fdd91e26df19908c76bdbdc2f5d
Kanin Tansirisittikul <tan.kanin98@gmail.com>
2021-09-06 14:08:27 +0700

    add tree spec

--- tree_visibility.rb
+++ tree_visibility.rb
@@ -1,32 +1,22 @@
 class TreeNode
   attr_accessor :val, :l, :r
 
-  def initialize(arr)
-    @val = arr[0]
-
-    @l =  if arr[1].is_a?(Array)
-            @l = TreeNode.new(arr[1])
-          else
-            @l = arr[1]
-          end
-
-    @r =  if arr[2].is_a?(Array)
-            @r = TreeNode.new(arr[2])
-          else
-            @r = arr[2]
-          end
+  def initialize(val, l = nil, r = nil)
+    @val = val
+    @l = l
+    @r = r
   end
 end
 
 class Tree
   def solution(tree)
-    return 0 if tree.nil?
+    return 0 if tree.val.nil?
 
     traverse(tree, tree.val)
   end
-  
+
   def traverse(tree, max)
-    tree.val >= max ? calculate_sum(1, tree, tree.val) : calculate_sum(0, tree, tree.val)
+    tree.val >= max ? calculate_sum(1, tree, tree.val) : calculate_sum(0, tree, max)
   end
 
   def calculate_sum(sum, tree, val)
