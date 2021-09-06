cf7365a0e063a0872fc8fde40f0d105316735454
Kanin Tansirisittikul <tan.kanin98@gmail.com>
2021-09-06 22:33:43 +0700

    better format

--- spec/day_of_week_spec.rb
+++ spec/day_of_week_spec.rb
@@ -4,48 +4,48 @@ require './day_of_week'
 describe DayOfWeek do
   describe '#solution' do
     subject { instance.solution }
-    let(:instance) { described_class.new('Wed', 2) }
 
-    context 'with given example' do
+    context 'with first example' do
+      let(:instance) { described_class.new('Wed', 2) }
+
       it 'returns Fri given s = Wed and k = 2' do
         expect(subject).to match('Fri')
       end
+    end
+
+    context 'with second example' do
+      let(:instance) { described_class.new('Sat', 23) }
 
       it 'returns Mon given s = Sat and k = 23' do
-        instance.day = 'Sat'
-        instance.k = 23
         expect(subject).to match('Mon')
       end
     end
 
-    context 'with same day' do
-      it 'returns itself' do
-        instance.day = 'Mon'
-        instance.k = 0
-        expect(subject).to match('Mon')
-      end
-    end
+    context 'with samll k where k < 7' do
+      let(:instance) { described_class.new('Wed', 0) }
 
-    context 'within same week' do
-      it 'returns Wed given s = Mon and k = 2' do
-        instance.day = 'Mon'
-        instance.k = 2
+      it 'returns itself when k = 0' do
         expect(subject).to match('Wed')
+      end
+
+      it 'returns Wed when k = 2' do
+        instance.k = 2
+        expect(subject).to match('Fri')
       end
     end
 
     context 'with next week where k < 7' do
+      let(:instance) { described_class.new('Sun', 3) }
+
       it 'returns Wed given s = Sun and k = 3' do
-        instance.day = 'Sun'
-        instance.k = 3
         expect(subject).to match('Wed')
       end
     end
 
     context 'with Big where k >= 7' do
+      let(:instance) { described_class.new('Wed', 500) }
+
       it 'returns Sat given s = Wed and k = 500' do
-        instance.day = 'Wed'
-        instance.k = 500
         expect(subject).to match('Sat')
       end
     end
