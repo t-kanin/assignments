e7ecd353ccd0c22df0509f54d7f74ba841f7e691
Kanin Tansirisittikul <61454485+t-kanin@users.noreply.github.com>
2021-09-06 16:00:22 +0700

    Assignment 5 (#6)
    * add assignment_1
    
    * init assignment_2
    
    * add assignment 2
    
    * work only with small sizes
    
    * remove files
    
    * add comment & test git
    
    * git init
    
    * complete assignment_5
    
    * fixed from comments + better format
    
    * remove setting file
    
    * delete ,
    
    * better way\ to find available agents
    
    * clean up code

---
+++ assignment_5.rb
@@ -0,0 +1,53 @@
+class AgentNotFoundException < StandardError; end
+
+class Ticket
+  attr_reader :id, :restrictions
+
+  def initialize(id, restrictions)
+    @id = id
+    @restrictions = restrictions
+  end
+
+  def print
+    puts "Ticket #{id}: require #{restrictions}"
+  end
+end
+
+class Agent
+  attr_reader :name, :skills, :load
+
+  def initialize(name, skills, load = 0)
+    @name = name
+    @skills = skills
+    @load = load
+  end
+
+  def print
+    puts "Agent #{name} has #{skills} skills and processes #{load} tickets "
+  end
+
+  def print_available(ticket_id)
+    @load += 1
+    puts "Agent #{name} has taken ticket number: #{ticket_id}"
+  end
+end
+
+class LeastLoadedAgent
+  def find(ticket, agents)
+    available_agents = agents.select { |agent| agent.skills.include?(ticket.restrictions) && agent.load < 3 }
+    return puts 'No agent available' if available_agents.empty?
+
+    available_agents.sort_by!(&:load)
+    available_agents.first.print_available(ticket.id)
+  end
+end
+
+class LeastFlexibleAgent
+  def find(ticket, agents)
+    available_agents = agents.select { |agent| agent.skills.include?(ticket.restrictions) && agent.load < 3 }
+    return puts 'No agent available' if available_agents.empty?
+
+    available_agents.sort_by!(&:skills)
+    available_agents.first.print_available(ticket.id)
+  end
+end
