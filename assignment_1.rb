fe639753a4879d966a6b66b0cb01d6f0d345e1f5
Kanin Tansirisittikul <tan.kanin98@gmail.com>
2021-09-06 08:48:44 +0700

    update format

--- assignment_1.rb
+++ assignment_1.rb
@@ -1,24 +1,23 @@
-def solution(number) 
+def solution(number)
   arr = []
   return number if number < 10
 
-  loop do 
-    arr << number%10 
+  loop do
+    arr << number % 10
     number /= 10
     break if number.zero?
-  end 
-	
+  end
+
   arr.sort.reverse.join.to_i
-end 
+end
 
-#test 4 digits
-p test1 = solution(9687) #expect 9876 got 9876
-p test2 = solution(2000) #expect 2000 got 2000
-p test3 = solution(5445) #expect 5544 got 5544
+# test 4 digits
+p solution(9687) # expect 9876 got 9876
+p solution(2000) # expect 2000 got 2000
+p solution(5445) # expect 5544 got 5544
 
-# test 1 digit 
-p test4 = solution(3) #expect 3 got 3 
-p test0 = solution(0) #expect 0 got 0 
+p solution(3) # expect 3 got 3
+p solution(0) # expect 0 got 0
 
 # test 2 digits
-p test5 = solution(45) # expect 54 got 54
+p solution(45) # expect 54 got 54
